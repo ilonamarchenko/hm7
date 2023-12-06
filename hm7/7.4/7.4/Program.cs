77b8d8743f4df5c3daa01485eea8a81aa102e69c
ilonamarchenko <136097417+ilonamarchenko@users.noreply.github.com>
2023-12-06 21:07:36 +0200

    Add files via upload

---
+++ hm7/7.4/7.4/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+public abstract class ChrstTree
+{
+    public virtual string Decorate()
+    {
+        return "Ялинка в тiльтi";
+    }
+}
+
+// Конкретний компонент
+public class ChristmasTree : ChrstTree
+{
+    public override string Decorate()
+    {
+        return base.Decorate(); // повертає "Christmas tree"
+    }
+}
+
+// Абстрактний декоратор
+public abstract class TreeDecorator : ChrstTree
+{
+    protected ChrstTree tree;
+
+    public TreeDecorator(ChrstTree tree)
+    {
+        this.tree = tree;
+    }
+
+    public override string Decorate()
+    {
+        // Делегування операції до об'єкта, який ми декоруємо
+        return tree.Decorate();
+    }
+}
+
+// Конкретний декоратор для прикрас
+public class BallsDecorator : TreeDecorator
+{
+    public BallsDecorator(ChrstTree tree) : base(tree)
+    {
+    }
+
+    public override string Decorate()
+    {
+        // Додавання нової поведінки
+        return base.Decorate() + DecorateWithTinsel();
+    }
+
+    private string DecorateWithTinsel()
+    {
+        return " ,вже не в тiльтi, вона тепер крута з iграшками";
+    }
+}
+
+// Конкретний декоратор для гірлянд
+public class LightsDecorator : TreeDecorator
+{
+    public LightsDecorator(ChrstTree tree) : base(tree)
+    {
+    }
+
+    public override string Decorate()
+    {
+        // Додавання нової поведінки
+        return base.Decorate() + DecorateWithLights();
+    }
+
+    private string DecorateWithLights()
+    {
+        return " та ще й з гiрляндами";
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        ChrstTree tree = new ChristmasTree();
+        Console.WriteLine(tree.Decorate());
+
+        // Додавання прикрас на ялинку
+        tree = new BallsDecorator(tree);
+        Console.WriteLine(tree.Decorate());
+
+        // Додавання гірлянд на ялинку
+        tree = new LightsDecorator(tree);
+        Console.WriteLine(tree.Decorate());
+    }
+}
---
+++ hm7/7.6/7.6/Program.cs
@@ -0,0 +1,182 @@
+﻿//factory method
+/*public interface Car
+{
+    void DisplayInfo();
+}
+
+public class BMW3Series : Car
+{
+    private double engineVolume;
+    private string driveType;
+
+    public BMW3Series(double engineVolume, string driveType)
+    {
+        this.engineVolume = engineVolume;
+        this.driveType = driveType;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"BMW 3 Series - executive car. Engine volume: {engineVolume}L, Drive type: {driveType}.");
+    }
+}
+
+public class BMW5Series : Car
+{
+    private double engineVolume;
+    private string driveType;
+
+    public BMW5Series(double engineVolume, string driveType)
+    {
+        this.engineVolume = engineVolume;
+        this.driveType = driveType;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"BMW 5 Series - mid-sized luxury car. Engine volume: {engineVolume}L, Drive type: {driveType}.");
+    }
+}
+
+public class BMW7Series : Car
+{
+    private double engineVolume;
+    private string driveType;
+
+    public BMW7Series(double engineVolume, string driveType)
+    {
+        this.engineVolume = engineVolume;
+        this.driveType = driveType;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"BMW 7 Series - full-sized luxury sedan. Engine volume: {engineVolume}L, Drive type: {driveType}.");
+    }
+}
+
+public abstract class CarFactory
+{
+    public abstract Car CreateCar();
+}
+
+public class BMW3SeriesFactory : CarFactory
+{
+    private double engineVolume;
+    private string driveType;
+
+    public BMW3SeriesFactory(double engineVolume, string driveType)
+    {
+        this.engineVolume = engineVolume;
+        this.driveType = driveType;
+    }
+
+    public override Car CreateCar()
+    {
+        return new BMW3Series(engineVolume, driveType);
+    }
+}
+
+public class BMW5SeriesFactory : CarFactory
+{
+    private double engineVolume;
+    private string driveType;
+
+    public BMW5SeriesFactory(double engineVolume, string driveType)
+    {
+        this.engineVolume = engineVolume;
+        this.driveType = driveType;
+    }
+
+    public override Car CreateCar()
+    {
+        return new BMW5Series(engineVolume, driveType);
+    }
+}
+
+public class BMW7SeriesFactory : CarFactory
+{
+    private double engineVolume;
+    private string driveType;
+
+    public BMW7SeriesFactory(double engineVolume, string driveType)
+    {
+        this.engineVolume = engineVolume;
+        this.driveType = driveType;
+    }
+
+    public override Car CreateCar()
+    {
+        return new BMW7Series(engineVolume, driveType);
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        CarFactory factory = new BMW3SeriesFactory(2.0, "RWD");
+        Car car3Series = factory.CreateCar();
+        car3Series.DisplayInfo();
+
+        factory = new BMW5SeriesFactory(3.0, "AWD");
+        Car car5Series = factory.CreateCar();
+        car5Series.DisplayInfo();
+
+        factory = new BMW7SeriesFactory(4.4, "AWD");
+        Car car7Series = factory.CreateCar();
+        car7Series.DisplayInfo();
+    }
+}*/
+
+//composite
+public interface UniversityComponent
+{
+    void DisplayInformation();
+}
+
+public class Faculty : UniversityComponent
+{
+    private List<UniversityComponent> components = new List<UniversityComponent>();
+    public string Name { get; }
+
+    public Faculty(string name)
+    {
+        Name = name;
+    }
+
+    public void AddComponent(UniversityComponent component)
+    {
+        components.Add(component);
+    }
+
+    public void RemoveComponent(UniversityComponent component)
+    {
+        components.Remove(component);
+    }
+
+    public void DisplayInformation()
+    {
+        Console.WriteLine("Faculty: " + Name);
+        foreach (var component in components)
+        {
+            component.DisplayInformation();
+        }
+    }
+}
+
+public class Teacher : UniversityComponent
+{
+    public string Name { get; }
+    public string Title { get; }
+
+    public Teacher(string name, string title)
+    {
+        Name = name;
+        Title = title;
+    }
+
+    public void DisplayInformation()
+    {
+        Console.WriteLine($"Teacher: {Name}, Title: {Title}");
+    }
+}
